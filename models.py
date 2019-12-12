325f731bba06f7c7a0d85e75b35d65511d3b1983
jay71294-dev <jpatels71294@gmail.com>
2019-12-12 15:15:36 -0500

    Successfully done flask

---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class Cats(db.Model):
+    __tablename__ = 'cats'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    breed = db.Column(db.String(100))
